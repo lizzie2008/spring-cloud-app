69ec8539fd1e9ca52e1a000141d8e3cce8d3b196
lizzie2008 <lizzie_hls@yeah.net>
2020-01-03 10:44:40 +0800

    add stream-rabbit

---
+++ config-server/src/test/java/tech/lancelot/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.lancelot.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
