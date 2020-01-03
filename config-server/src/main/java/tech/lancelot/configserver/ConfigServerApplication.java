a3cf5bf3296fd1f8934daf478e9e278bd5ba1abf
lizzie2008 <lizzie_hls@yeah.net>
2020-01-03 11:24:56 +0800

    add stream-rabbit

--- config-server/src/main/java/tech/lancelot/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/tech/lancelot/configserver/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package tech.lancelot.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
