6b0c3d94c78a84ecc3e90d8dab184ec6dc6a3dc5
xuli <xuli1@cofco.com>
2020-07-16 12:58:36 +0800

    modify configuration/ZuulConfiguration.java

--- api-gateway/src/main/java/tech/lancelot/apigateway/configuration/ZuulConfiguration.java
+++ api-gateway/src/main/java/tech/lancelot/apigateway/configuration/ZuulConfiguration.java
@@ -3,9 +3,11 @@ package tech.lancelot.apigateway.configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
 @Component
+@EnableAspectJAutoProxy
 public class ZuulConfiguration {
 
     @ConfigurationProperties("zuul")
